bbda4f177eafb17e4cd1de9dd7412937f983f876
Sanjay-Kr-Sharma <39439644+Sanjay-Kr-Sharma@users.noreply.github.com>
2018-05-20 09:39:11 +0530

    Initial check in...

---
+++ SampleWebApi/SampleDataLayer/ContactModel.cs
@@ -0,0 +1,34 @@
+namespace SampleDataLayer
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ContactModel : DbContext
+    {
+        
+        Type providerService = typeof(System.Data.Entity.SqlServer.SqlProviderServices);
+        public ContactModel()
+            : base("name=ContactModel")
+        {
+        }
+
+        public virtual DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>()
+                .Property(e => e.PrimaryPhone)
+                .HasPrecision(18, 0);
+
+            modelBuilder.Entity<Contact>()
+                .Property(e => e.Phone1)
+                .HasPrecision(18, 0);
+
+            modelBuilder.Entity<Contact>()
+                .Property(e => e.Phone2)
+                .HasPrecision(18, 0);
+        }
+    }
+}
---
+++ SampleWebApi/SampleWebApi/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Security;
+
+namespace SampleWebApi.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public string Get()
+        {
+            FormsAuthentication.SetAuthCookie("Badri", false);
+            return "Redirecting to login page...";
+        }
+
+        public void Post()
+        {
+            FormsAuthentication.SetAuthCookie("Badri", false);
+        }
+    }
+}
